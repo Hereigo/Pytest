ecdb656de53a6968c5de445311d3181661362375
Hereigo <hereigo@online.ua>
2024-05-07 20:57:46 +0300

    update 1

--- aaa.py
+++ aaa.py
@@ -1,8 +1,29 @@
-x = "Python"
-y = "is"
-z = "awesome"
-print("Result - ", x, y, z)
-print("'", x, "' is ", type(x))
-for x in range(5):
-    print("aaa-", x)
+class Car:
+    speed = 0
+    started = False
 
+    def start(self):
+        self.started = True
+        print("Car started, let's ride!")
+
+    def increase_speed(self, delta):
+        if self.started:
+            self.speed = self.speed + delta
+            print("Vrooooom!")
+        else:
+            print("You need to start the car first")
+
+    def stop(self):
+        self.speed = 0
+        print("Halting")
+
+
+car = Car()
+car.increase_speed(10)
+car.start()
+car.increase_speed(40)
+
+# ===================================
+# You need to start the car first
+# Car started, let's ride!
+# Vrooooom!
