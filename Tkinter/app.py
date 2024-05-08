bc60688b4c41bab7a06661ab05c4749c6cd31ae0
Hereigo <hereigo@online.ua>
2024-05-08 13:45:22 +0300

    app py update

--- Tkinter/app.py
+++ Tkinter/app.py
@@ -6,18 +6,35 @@ from tkinter import messagebox
 class MyApp:
     def __init__(self, master):
         self.master = master
+        self.master.after(4000, self.master.focus_force())
+        self.master.attributes()
+        self.master.bind()
+        self.master.config()
+        # self.master.destroy()
+        # self.master.focus_force()
+        # MainWindow Geometry: H х W + L + R
+        self.master.geometry("600x400+600+300")
+        self.master.grab_set()
+        self.master.iconbitmap(
+            # ===> _tkinter.TclError: format error in bitmap data
+            # os.path.dirname(os.path.abspath(__file__)) + "/tkinter.ico"
+        )
+        self.master.lift()
+        # self.master.lower() # Open behind of all windows
+        self.master.maxsize()
+        self.master.minsize()
+        self.master.protocol()
+        self.master.resizable(False, False)
+        self.master.state()  # Maximized, Minimized, ...
         self.master.title("My App")
-        self.master.geometry("600x400")
 
         self.lbl1 = Label(
             self.master,
             text="Bla-bla-bla very-very-very-very important text bla-bla-bla ...",
         )
         self.lbl1.pack()
-
         self.btn1 = Button(self.master, text="Show MessageBox", command=self.show_msg)
         self.btn1.pack()
-
         self.btn2 = Button(self.master, text="Exit App", command=self.exit_app)
         self.btn2.pack()
 
@@ -35,9 +52,4 @@ class MyApp:
 if __name__ == "__main__":
     tkWindow = Tk()
     app = MyApp(tkWindow)
-
-    # ===> _tkinter.TclError: format error in bitmap data
-    # icon = BitmapImage(file=os.path.dirname(os.path.abspath(__file__)) + "/tkinter.ico")
-    # tkWindow.iconbitmap(icon)
-
     tkWindow.mainloop()
