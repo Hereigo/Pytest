ee2fd353f7832e075501a3a696132008216db168
Hereigo <hereigo@online.ua>
2024-05-08 12:16:51 +0300

    app py update

--- Tkinter/app.py
+++ Tkinter/app.py
@@ -3,28 +3,41 @@ from tkinter import *
 from tkinter import messagebox
 
 
-def clicked1():
-    messagebox.showinfo("Messagebox", "Hello, I'm MessageBox!")
+class MyApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("My App")
+        self.master.geometry("600x400")
 
+        self.lbl1 = Label(
+            self.master,
+            text="Bla-bla-bla very-very-very-very important text bla-bla-bla ...",
+        )
+        self.lbl1.pack()
 
-def clicked2():
-    root.destroy()
+        self.btn1 = Button(self.master, text="Show MessageBox", command=self.show_msg)
+        self.btn1.pack()
+
+        self.btn2 = Button(self.master, text="Exit App", command=self.exit_app)
+        self.btn2.pack()
+
+        self.lbl1.place(x=10, y=10)
+        self.btn1.place(x=175, y=350)
+        self.btn2.place(x=325, y=350)
+
+    def show_msg(self):
+        messagebox.showinfo("Messagebox", "Hello, I'm MessageBox!")
+
+    def exit_app(self):
+        self.master.destroy()
 
 
 if __name__ == "__main__":
-    root = Tk()
+    tkWindow = Tk()
+    app = MyApp(tkWindow)
 
-    lbl1 = Label(root, text="Bla-bla-bla very important text ...")
-    btn1 = Button(root, text="Show MessageBox", command=clicked1)
-    btn2 = Button(root, text="Exit App", command=clicked2)
-
-    lbl1.place(x=10, y=10)
-    btn1.place(x=175, y=350)
-    btn2.place(x=325, y=350)
-
-    root.title("My App")
-    root.geometry("600x400")
+    # ===> _tkinter.TclError: format error in bitmap data
     # icon = BitmapImage(file=os.path.dirname(os.path.abspath(__file__)) + "/tkinter.ico")
-    # root.iconbitmap(icon)
+    # tkWindow.iconbitmap(icon)
 
-    root.mainloop()
+    tkWindow.mainloop()
